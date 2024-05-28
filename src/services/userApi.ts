36ee2788acebbeb32dffdf6d763180cac47afcd1
hamer orlando albarran valderrama <hameralbarran@gmail.com>
2024-05-27 19:14:31 -0500

    se modificio la ruta a la api

--- src/services/userApi.ts
+++ src/services/userApi.ts
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const userApi = createApi({
   reducerPath: 'userApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://yocreoquesipuedohacerlo.com/juegoDeRifas/'
+    baseUrl: 'https://yocreoquesipuedohacerlo.com/juegoDeRifasDaniel/'
   }),
   // keepUnusedDataFor: 3,
   endpoints: (builder) => ({
